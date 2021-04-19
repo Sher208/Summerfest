e10e79ff3351dbf11e2858645d32c36d2e1a5dec
Thilakraj Shetty H <thilakrajshetty08@gmail.com>
2021-04-19 19:12:51 +0530

    Dependencies removed

--- server/src/main/java/com/summerfest/server/Controller/UserController.java
+++ server/src/main/java/com/summerfest/server/Controller/UserController.java
@@ -1,17 +1,11 @@
 package com.summerfest.server.Controller;
 
 import java.security.Principal;
-
-import javax.validation.Valid;
-
-import com.summerfest.server.Model.User;
 import com.summerfest.server.Service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
